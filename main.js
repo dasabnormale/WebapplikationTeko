91944efdaf4c0f1ff20bb2c3b3dbd6fde5570624
dasabnormale <patsch@bluewin.ch>
2025-08-11 21:46:10 +0200

    API Hinzugefügt. index HTML angepasst

---
+++ main.js
@@ -0,0 +1,5 @@
+const feld1 = document.getElementById("Start");
+const feld2 = document.getElementById("End");
+const btn = document.getElementById("submit");
+const ergebnis = document.getElementById("ergebnis");
+
